0978e6dc3986ff306a7648ad850b9c435038103d
Mad W. Hatter <74023373+mwhatter@users.noreply.github.com>
2023-08-12 07:39:43 -0600

    Update small_dol_recon.ps1
    added host and user groups to output xlsx

--- Tools/Scripts/small_dol_recon.ps1
+++ Tools/Scripts/small_dol_recon.ps1
@@ -3,16 +3,20 @@ $computerName = if ($comboBoxComputerName.SelectedItem) {
 } else {$comboBoxComputerName.Text}
 if (![string]::IsNullOrEmpty($computerName)) {
     try {
+        New-Item -ItemType Directory -Path ".\Logs\Reports\$computerName\ADRecon" -Force | Out-Null
         # Gather system info
         $systemInfo = Get-CimInstance -ClassName Win32_ComputerSystem -ComputerName $computerName | Select-Object -Property Name,PrimaryOwnerName,Domain,Model,Manufacturer 
         $textboxResults.AppendText("System Information: `r`n$( $systemInfo | Out-String)")
+        $systemInfo | Export-Excel -Path ".\Logs\Reports\$computerName\ADRecon\SysUserADInfo.xlsx"-WorksheetName 'System Info' -AutoSize -AutoFilter -TableStyle Medium6
 
         # Gather specified AD host info
         $adEntry = Get-ADComputer -Filter "Name -eq '$computerName'" -Properties Name, DNSHostName, IPv4Address, Created, Description, DistinguishedName, Enabled, OperatingSystem, OperatingSystemVersion, SID
         $textboxResults.AppendText("Host AD Information: `r`n$( $adEntry | Out-String)")
+        $adEntry | Export-Excel -Path ".\Logs\Reports\$computerName\ADRecon\SysUserADInfo.xlsx" -WorksheetName 'Host AD Info' -AutoSize -AutoFilter -TableStyle Medium6
 
         # Fetch AD groups the computer is a member of
         $computerGroups = Get-ADPrincipalGroupMembership -Identity $adEntry.DistinguishedName
+        $computerGroups | Export-Excel -Path ".\Logs\Reports\$computerName\ADRecon\SysUserADInfo.xlsx" -WorksheetName 'Host Groups' -AutoSize -AutoFilter -TableStyle Medium6 
 
         # Format the group names as a bulleted list
         $computerGroupNames = "- " + ($computerGroups.Name -join "`r`n- ")
@@ -36,10 +40,12 @@ if (![string]::IsNullOrEmpty($computerName)) {
             try {
                 $adUserEntry = Get-ADUser -Identity $user.Name -Properties SamAccountName, whenCreated, Description, Enabled, GivenName, Surname, DisplayName, EmailAddress
                 $textboxResults.AppendText("User AD Information for $($user.Name) `r`n$( $adUserEntry | Out-String)")
+                $adUserEntry | Export-Excel -Path ".\Logs\Reports\$computerName\ADRecon\SysUserADInfo.xlsx" -WorksheetName 'User AD Info' -AutoSize -AutoFilter -TableStyle Medium6 -Append
 
                 # Fetch AD groups the user is a member of
                 $userGroups = Get-ADPrincipalGroupMembership -Identity $user.Name
                 $userGroupNames = "- " + ($userGroups.Name -join "`r`n- ")
+                $userGroups | Export-Excel -Path ".\Logs\Reports\$computerName\ADRecon\SysUserADInfo.xlsx" -WorksheetName 'User Groups' -AutoSize -AutoFilter -TableStyle Medium6 -Append
 
                 # Count the number of groups the user is a member of
                 $userGroupCount = $userGroups.Count
@@ -58,14 +64,13 @@ if (![string]::IsNullOrEmpty($computerName)) {
             }
         }
 
-        New-Item -ItemType Directory -Path ".\Logs\Reports\$computerName\ADRecon" -Force | Out-Null
+        $data = Import-Excel -Path ".\Logs\Reports\$computerName\ADRecon\SysUserADInfo.xlsx" -WorksheetName 'User Groups'
 
-        # Export to Excel
-        $systemInfo | Export-Excel -Path ".\Logs\Reports\$computerName\ADRecon\SysUserADInfo.xlsx"-WorksheetName 'System Info' -AutoSize -AutoFilter -TableStyle Medium6
-        $adEntry | Export-Excel -Path ".\Logs\Reports\$computerName\ADRecon\SysUserADInfo.xlsx" -WorksheetName 'Host AD Info' -AutoSize -AutoFilter -TableStyle Medium6 -Append
-        $adUserEntries | Export-Excel -Path ".\Logs\Reports\$computerName\ADRecon\SysUserADInfo.xlsx" -WorksheetName 'User AD Info' -AutoSize -AutoFilter -TableStyle Medium6 -Append
+        # Deduplicate the data
+        $deduplicatedData = $data | Select-Object * -Unique
 
-        # Print completion message
+        # Write the deduplicated data back to the Excel worksheet, overwriting the original
+        $deduplicatedData | Export-Excel -Path ".\Logs\Reports\$computerName\ADRecon\SysUserADInfo.xlsx" -WorksheetName 'User Groups' -AutoSize -AutoFilter -TableStyle Medium6 -ClearSheet
         $reconstart = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
         $textboxResults.AppendText("System and User AD Information exported to Excel workbook at $reconstart`r`n")
         Write-Host "System and User AD Information exported to Excel workbook at $reconstart" -ForegroundColor Green 

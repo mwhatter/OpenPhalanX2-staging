2d918e48ebfeb7cfafbcb06ec5bf1903eb7efdf8
Mad W. Hatter <74023373+mwhatter@users.noreply.github.com>
2023-08-13 13:55:34 -0600

    Update small_dol_logoff.ps1

--- Tools/Scripts/small_dol_logoff.ps1
+++ Tools/Scripts/small_dol_logoff.ps1
@@ -10,7 +10,20 @@ $computerName = if ($comboBoxComputerName.SelectedItem) {
     $comboBoxComputerName.Text
 }
 
-$result = Invoke-Command -ComputerName $computerName -ScriptBlock {
-    & query user
+# Get the session ID for the selected user
+$sessionData = Invoke-Command -ComputerName $computerName -ScriptBlock {
+    $output = & query user $args[0]
+    $sessionLine = $output | Where-Object { $_ -like "*$args[0]*" }
+    $sessionId = ($sessionLine -split "\s+")[2]
+    return $sessionId
+} -ArgumentList $selectedUser
+
+# Log off the user with the retrieved session ID
+if ($sessionData) {
+    Invoke-Command -ComputerName $computerName -ScriptBlock {
+        & logoff $args[0]
+    } -ArgumentList $sessionData
+    Write-Host "$selectedUser has been logged off from $computerName"
+} else {
+    Write-Host "Could not find session ID for user $selectedUser on $computerName"
 }
-$result

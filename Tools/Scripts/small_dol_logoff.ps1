a42c331216674e562cfd7fe20d9a487a0d5dba2c
Mad W. Hatter <74023373+mwhatter@users.noreply.github.com>
2023-08-13 13:39:00 -0600

    Update small_dol_logoff.ps1

--- Tools/Scripts/small_dol_logoff.ps1
+++ Tools/Scripts/small_dol_logoff.ps1
@@ -9,13 +9,59 @@ $computerName = if ($comboBoxComputerName.SelectedItem) {
     $comboBoxComputerName.Text
 }
 
-Invoke-Command -ComputerName $computerName -ScriptBlock {
-    $username = $args[0]
-    $sessions = query user | Where-Object { $_ -match "\s+(\d+)\s+($username)" } | ForEach-Object { if ($matches[1]) { $matches[1] } }
-    foreach ($sessionId in $sessions) {
-        logoff $sessionId
+# Function to remotely execute commands and return the result
+function Execute-RemoteCommand {
+    param (
+        [string]$ComputerName,
+        [string]$Command
+    )
+    try {
+        $result = Invoke-Command -ComputerName $ComputerName -ScriptBlock {
+            param($Command)
+            
+            $processInfo = New-Object System.Diagnostics.ProcessStartInfo
+            $processInfo.FileName = "powershell.exe"
+            $processInfo.Arguments = "-NoProfile -ExecutionPolicy Bypass -Command $Command"
+            $processInfo.RedirectStandardOutput = $true
+            $processInfo.RedirectStandardError = $true
+            $processInfo.UseShellExecute = $false
+            $processInfo.CreateNoWindow = $true
+
+            $process = New-Object System.Diagnostics.Process
+            $process.StartInfo = $processInfo
+            $process.Start() | Out-Null
+
+            $stdout = $process.StandardOutput.ReadToEnd()
+            $stderr = $process.StandardError.ReadToEnd()
+
+            $process.WaitForExit()
+
+            if ($stderr) {
+                return "Error: $stderr"
+            } elseif ($stdout) {
+                return $stdout
+            }
+
+            return $null
+        } -ArgumentList $Command
+
+        return $result
     }
-} -ArgumentList $selectedUser
+    catch {
+        return "Error executing command: $_"
+    }
+}
+
+# Fetch the session ID(s) of the specified user on the remote machine
+$sessionIdCommandOutput = Execute-RemoteCommand -ComputerName $computerName -Command "query user"
+
+$sessionIds = $sessionIdCommandOutput | Where-Object { $_ -match "\s+(\d+)\s+($selectedUser)" } | ForEach-Object { if ($matches[1]) { $matches[1] } }
+
+foreach ($sessionId in $sessionIds) {
+    # Logoff the user using the retrieved session ID on the remote machine
+    $logoffResult = Execute-RemoteCommand -ComputerName $computerName -Command "logoff $sessionId"
+    # You can use $logoffResult for any additional logging or processing as needed
+}
 
 $gotlogoffusr = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
 Write-Host "$Username logged $selectedUser off from $computerName at $gotlogoffusr" -ForegroundColor Cyan 

825343da93136b162c6c756d5374394b95781ea2
Mad W. Hatter <74023373+mwhatter@users.noreply.github.com>
2023-08-12 18:19:26 -0600

    Update Phalanx_Setup.ps1
    unified error handling

--- Phalanx_Setup.ps1
+++ Phalanx_Setup.ps1
@@ -1,15 +1,24 @@
+<#
+This script downloads and installs a variety of security tools, including PowerShell 7, 
+the vmray-rest-api Python module, DeepBlueCLI, Hayabusa, Sysmon, and the Get-ZimmermanTools 
+script. It also creates a set of directories to store the tools and their associated files.
+
+The script also writes the success or failure of each action to the console. This makes it 
+easier to track the progress of the script and troubleshoot any errors that may occur.
+#>
+
 $directories = @(
     ".\CopiedFiles",
     ".\Logs",
     ".\Logs\EVTX",
     ".\Logs\Reports",
     ".\Logs\Audit",
+    ".\Tools",
     ".\Tools\Hayabusa",
     ".\Tools\Sysmon",
     ".\Tools\EZTools"
 )
 
-# Initialize an array to store errors
 $errors = @()
 
 ################## Workspace ##################
@@ -19,150 +28,135 @@ foreach ($directory in $directories) {
         Write-Host "Directory $directory created successfully."
     } catch {
         $errors += [PSCustomObject]@{
-            'Step'   = "Creating $directory"
-            'Error'  = $_.Exception.Message
+            Step  = "Creating $directory"
+            Error = $_.Exception.Message
         }
     }
 }
 
 ################## Downloads ##################
-# Define the URL for the PowerShell 7 MSI installer
+# PowerShell 7
 $downloadUrl = "https://github.com/PowerShell/PowerShell/releases/download/v7.3.5/PowerShell-7.3.5-win-x64.msi"
-
-# Extract the filename from the download URL
 $fileName = $downloadUrl -split '/' | Select-Object -Last 1
-
-# Define the path to save the MSI file
 $savePath = Join-Path -Path $env:TEMP -ChildPath $fileName
 
 try {
-    # Download the MSI file
     Invoke-WebRequest -Uri $downloadUrl -OutFile $savePath
-
-    Write-Host "PowerShell 7 MSI download successful."
-
-    # Install PowerShell 7 using the downloaded MSI file
+    Write-Host "PowerShell 7 MSI downloaded successfully."
     Start-Process -FilePath msiexec.exe -ArgumentList "/i `"$savePath`" /qn" -Wait
-
-    # Clean up the temporary MSI file
     Remove-Item -Path $savePath
-
-    Write-Host "PowerShell 7 installation successful."
+    Write-Host "PowerShell 7 installed successfully."
 } catch {
-    $installError = $_
-    Write-Host "An error occurred during installation: $installError"
+    $errors += [PSCustomObject]@{
+        Step  = "Installing PowerShell 7"
+        Error = $_.Exception.Message
+    }
 }
 
-# Define the URL for the Python download page
+# Python
 $downloadUrl = "https://www.python.org/ftp/python/3.11.4/python-3.11.4-amd64.exe"
+$installerPath = Join-Path -Path c:\Test -ChildPath "python_installer.exe"
 
-# Retrieve the download page
-$response = Invoke-WebRequest -Uri $downloadUrl
-
-# Extract the download URL for the latest stable release of Python 3
-if ($response) {
-    # Define the path to save the installer
-    $installerPath = Join-Path -Path c:\Test -ChildPath "python_installer.exe"
-
-    # Download the installer
+try {
     Invoke-WebRequest -Uri $downloadUrl -OutFile $installerPath
-
-    # Install Python for all users
     Start-Process -FilePath $installerPath -ArgumentList "/quiet", "TargetDir=C:\Python", "AddToPath=1", "AssociateFiles=1", "Shortcuts=1" -Wait
-
-    # Clean up the installer
     Remove-Item -Path $installerPath
-    Write-Host "Python downloaded and installed successfully."
+    Write-Host "Python installed successfully."
+} catch {
+    $errors += [PSCustomObject]@{
+        Step  = "Installing Python"
+        Error = $_.Exception.Message
+    }
 }
-else {
-    Write-Host "Unable to retrieve the download link for Python."
-    $downloadError = $_
-}
- 
-# Install the numscrypt Python module
+
+# numscrypt Python module
 try {
     Start-Process -FilePath python.exe -ArgumentList "-m pip install numscrypt" -Wait
-    Write-Host "numscrypt Python module installation successful."
+    Write-Host "numscrypt Python module installed successfully."
 } catch {
-    $installError = $_
+    $errors += [PSCustomObject]@{
+        Step  = "Installing numscrypt Python module"
+        Error = $_.Exception.Message
+    }
 }
 
-# Install the vmray-rest-api Python module
+# vmray-rest-api Python module
 try {
     Start-Process -FilePath python.exe -ArgumentList "-m pip install vmray-rest-api" -Wait
-    Write-Host "vmray-rest-api Python module installation successful."
+    Write-Host "vmray-rest-api Python module installed successfully."
 } catch {
-    $installError = $_
+    $errors += [PSCustomObject]@{
+        Step  = "Installing vmray-rest-api Python module"
+        Error = $_.Exception.Message
+    }
 }
 
-################## Download and unzip DeepBlueCLI repository ##################
+# DeepBlueCLI
 try {
-    $deepBlueCLIZip = Invoke-WebRequest -URI https://github.com/sans-blue-team/DeepBlueCLI/archive/refs/heads/master.zip -OutFile DeepBlueCLI.zip
-    Expand-Archive DeepBlueCLI.zip -DestinationPath ".\Tools\" -Force
-    Remove-Item DeepBlueCLI.zip
-
-    # Remove existing DeepBlueCLI directory if it exists
+    $deepBlueCLIZip = Invoke-WebRequest -URI "https://github.com/sans-blue-team/DeepBlueCLI/archive/refs/heads/master.zip" -OutFile "DeepBlueCLI.zip"
+    Expand-Archive "DeepBlueCLI.zip" -DestinationPath ".\Tools\" -Force
+    Remove-Item "DeepBlueCLI.zip"
     if (Test-Path .\Tools\DeepBlueCLI) {
         Remove-Item .\Tools\DeepBlueCLI -Recurse -Force
     }
-
     Rename-Item .\Tools\DeepBlueCLI-master -NewName DeepBlueCLI -Force | Out-Null
-
-    Write-Host "DeepBlueCLI download and unzip successful."
+    Write-Host "DeepBlueCLI installed successfully."
 } catch {
-    Write-Host "DeepBlueCLI download and unzip failed:`n$_"
-    $downloadError = $_
+    $errors += [PSCustomObject]@{
+        Step  = "Installing DeepBlueCLI"
+        Error = $_.Exception.Message
+    }
 }
 
-################## Get the latest Hayabusa release page ##################
+# Hayabusa
 $hayabusaApiUrl = "https://api.github.com/repos/Yamato-Security/hayabusa/releases/latest"
 $hayabusaReleaseData = Invoke-RestMethod -Uri $hayabusaApiUrl
-
-# Extract the download URL for the latest Hayabusa release
-$hayabusaDownloadUrl = $hayabusaReleaseData.assets |
-                       Where-Object { $_.browser_download_url -like "*-win-64-bit.zip" } |
-                       Select-Object -First 1 -ExpandProperty browser_download_url
-
-# Prepare the local paths for downloading and extracting the Hayabusa zip file
+$hayabusaDownloadUrl = $hayabusaReleaseData.assets | Where-Object { $_.browser_download_url -like "*-win-64-bit.zip" } | Select-Object -First 1 -ExpandProperty browser_download_url
 $hayabusaZipPath = Join-Path -Path ".\Tools" -ChildPath "hayabusa-win-x64.zip"
 $hayabusaExtractPath = ".\Tools\Hayabusa"
 
-################## Download and extract Hayabusa ##################
+# Download Hayabusa
 try {
     Invoke-WebRequest -Uri $hayabusaDownloadUrl -OutFile $hayabusaZipPath
-    Write-Host "Hayabusa download successful."
+    Write-Host "Hayabusa downloaded successfully."
 } catch {
-    Write-Host "Hayabusa download failed:`n$_"
-    $downloadError = $_
+    $errors += [PSCustomObject]@{
+        Step  = "Downloading Hayabusa"
+        Error = $_.Exception.Message
+    }
 }
 
-################## Extract the Hayabusa files ##################
+# Extract Hayabusa
 try {
     Expand-Archive -Path $hayabusaZipPath -DestinationPath $hayabusaExtractPath -Force
-    Write-Host "Hayabusa files extracted successfully."
+    Write-Host "Hayabusa extracted successfully."
 } catch {
-    Write-Host "Hayabusa files extraction failed:`n$_"
+    $errors += [PSCustomObject]@{
+        Step  = "Extracting Hayabusa"
+        Error = $_.Exception.Message
+    }
 }
 
-################## Rename the executable to hayabusa.exe ##################
+# Rename Hayabusa executable
 $hayabusaExecutable = Get-ChildItem -Path $hayabusaExtractPath -Filter "hayabusa-*" -Recurse -File | Select-Object -First 1
 $hayabusaExecutable | Rename-Item -NewName "hayabusa.exe" -Force | Out-Null
 
-################## Update the Hayabusa rules ##################
+# Update Hayabusa rules
 $rulesPath = ".Tools\Hayabusa\Rules"
 $hayabusaExecutablePath = Join-Path -Path $hayabusaExtractPath -ChildPath "hayabusa.exe"
 
 try {
     & $hayabusaExecutablePath update-rules -r $rulesPath | Out-Null
     Write-Host "Hayabusa rules updated successfully."
-
-    # Clean up the downloaded zip file
     Remove-Item -Path $hayabusaZipPath -Force
 } catch {
-    Write-Host "Hayabusa rules update failed:`n$_"
+    $errors += [PSCustomObject]@{
+        Step  = "Updating Hayabusa rules"
+        Error = $_.Exception.Message
+    }
 }
 
-################## Download and unzip Sysmon ##################
+# Sysmon
 $sysmonUrl = "https://download.sysinternals.com/files/Sysmon.zip"
 $sysmonconfigurl = "https://raw.githubusercontent.com/olafhartong/sysmon-modular/master/sysmonconfig.xml"
 $sysmonZip = ".\Tools\Sysmon.zip"
@@ -173,35 +167,33 @@ try {
     Invoke-WebRequest -URI $sysmonUrl -OutFile $sysmonZip
     Expand-Archive $sysmonZip -DestinationPath $sysmonPath -Force
     Remove-Item $sysmonZip
-
     Invoke-WebRequest -URI $sysmonconfigurl -OutFile $sysmonConfigPath
-
-    Write-Host "Sysmon and sysmonconfig download and unzip successful."
+    Write-Host "Sysmon and sysmonconfig downloaded and installed successfully."
 } catch {
-    Write-Host "Sysmon and sysmonconfig download and unzip failed:`n$_"
-    $downloadError = $_
+    $errors += [PSCustomObject]@{
+        Step  = "Installing Sysmon"
+        Error = $_.Exception.Message
+    }
 }
 
-################## Download and execute Get-ZimmermanTools to retrieve Eric Zimmerman's toolset ##################
+# Get-ZimmermanTools
 $zimmermanToolsZip = Invoke-WebRequest -URI "https://raw.githubusercontent.com/EricZimmerman/Get-ZimmermanTools/master/Get-ZimmermanTools.ps1" -OutFile ".\Tools\EZTools\Get-ZimmermanTools.ps1"
 
 try {
     unblock-file ".\Tools\EZTools\Get-ZimmermanTools.ps1"
     .\Tools\EZTools\Get-ZimmermanTools.ps1 -Dest ".\Tools\EZTools" -NetVersion 4 -Verbose:$false *> $null
-    Write-Host "Get-ZimmermanTools download and execution successful."
+    Write-Host "Get-ZimmermanTools installed successfully."
 } catch {
-    Write-Host "Get-ZimmermanTools download and execution failed:`n$_"
-    $downloadError = $_
+    $errors += [PSCustomObject]@{
+        Step  = "Installing Get-ZimmermanTools"
+        Error = $_.Exception.Message
+    }
 }
 
 ################## Errors ##################
 if ($errors.Count -gt 0) {
-    Write-Host "The following errors occurred:"
-    foreach ($error in $errors) {
-        Write-Host ("Step: " + $error.Step)
-        Write-Host ("Error: " + $error.Error)
-        Write-Host "------------------------------------"
-    }
+    Write-Host "Following errors occurred during the process:"
+    $errors | ForEach-Object { Write-Host "$($_.Step): $($_.Error)" }
 } else {
-    Write-Host "All downloads and installations successful."
+    Write-Host "All operations completed successfully."
 }
